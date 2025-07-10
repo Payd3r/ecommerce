f32cc85c69f9b4abe8e6728e5d2bcb9914ec6cf8
Payd3r <62106049+Payd3r@users.noreply.github.com>
2025-07-11 00:25:24 +0200

    e

--- Frontend/api/config.js
+++ Frontend/api/config.js
@@ -1,6 +1,6 @@
 
 export function getApiUrl() {
-    return window.APP_CONFIG.API_URL;
+    return 'https://api.panesalame.andrea-mauri.duckdns.org';
     
     // Ora puoi usare API_URL e MEDIA_URL nel resto del tuo codice;
 }
