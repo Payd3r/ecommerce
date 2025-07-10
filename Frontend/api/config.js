e6b823ac653b74cd25dc0c71b44582ef5271a2c1
Payd3r <62106049+Payd3r@users.noreply.github.com>
2025-07-11 00:20:08 +0200

    e

--- Frontend/api/config.js
+++ Frontend/api/config.js
@@ -1,5 +1,6 @@
-// Legge la configurazione dall'oggetto globale definito in index.html
-const API_URL = window.APP_CONFIG.API_URL;
 
-
-// Ora puoi usare API_URL e MEDIA_URL nel resto del tuo codice
+export function getApiUrl() {
+    return window.APP_CONFIG.API_URL;
+    
+    // Ora puoi usare API_URL e MEDIA_URL nel resto del tuo codice;
+}
